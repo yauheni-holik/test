743274d1655236f3c0456496a26c5f1b73a108ce
Yauheni Holik <yauheni.holik@startmatter.com>
2018-12-13 17:04:40 +0300

    Change style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,35 +1,40 @@
-@if($app_footer)
-{!! $app_footer !!}
+@if($appFooter)
+{!! $appFooter !!}
 @else
-<footer class="footer">
+<!--<footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-sm-5">
-                @if($show_support)
-                <p>{!! trans('cachet.powered_by') !!}</p>
+            <div class="col-sm-4">
+                @if($showSupport)
+                <p>
+                    {!! trans('cachet.powered_by') !!}
+                    @if($showTimezone)
+                    {{ trans('cachet.timezone', ['timezone' => $timezone]) }}
+                    @endif
+                </p>
                 @endif
             </div>
-            <div class="col-sm-7">
+            <div class="col-sm-8">
                 <ul class="list-inline">
-                    @if($current_user || Config::get('setting.dashboard_login_link'))
+                    @if($currentUser || $dashboardLink)
                     <li>
-                        <a class="btn btn-link" href="/dashboard">{{ trans('dashboard.dashboard') }}</a>
+                        <a class="btn btn-link" href="{{ cachet_route('dashboard') }}">{{ trans('dashboard.dashboard') }}</a>
                     </li>
                     @endif
-                    @if($current_user)
+                    @if($currentUser)
                     <li>
-                        <a class="btn btn-link" href="/auth/logout">{{ trans('dashboard.logout') }}</a>
+                        <a class="btn btn-link" href="{{ cachet_route('auth.logout') }}">{{ trans('dashboard.logout') }}</a>
                     </li>
                     @endif
                     <li>
-                        <a class="btn btn-link" href="{{ route('feed.rss') }}">{{ trans('cachet.rss-feed') }}</a>
+                        <a class="btn btn-link" href="{{ cachet_route('feed.rss') }}">{{ trans('cachet.rss-feed') }}</a>
                     </li>
                     <li>
-                        <a class="btn btn-link" href="{{ route('feed.atom') }}">{{ trans('cachet.atom-feed') }}</a>
+                        <a class="btn btn-link" href="{{ cachet_route('feed.atom') }}">{{ trans('cachet.atom-feed') }}</a>
                     </li>
-                    @if(subscribers_enabled())
+                    @if($enableSubscribers)
                     <li>
-                        <a class="btn btn-success btn-outline" href="{{ route('subscribe.subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
+                        <a class="btn btn-success btn-outline" href="{{ cachet_route('subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
                     </li>
                     @endif
                 </ul>
@@ -37,6 +42,47 @@
         </div>
     </div>
 </footer>
+-->
+<div id="footer">
+	<div class="container">
+		<div class="line-separator text-center"><img src="images/separator.png" alt="separator" class=""></div>
+		<div class="row">
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<h5 class="footer-title">Using Heroic</h5>
+				<ul class="footer-links">
+					<li><a href="https://app.heroicnow.com" class="">Log in to your Account</a></li>
+					<li><a href="https://heroicnow.com/start.php" class="">Create a New Account</a></li>
+				</ul>
+			</div>
+				<div class="col-sm-6 col-md-4 col-lg-3">
+				<h5 class="footer-title">Help & Support</h5>
+				<ul class="footer-links">
+					<li><a href="https://heroicnow.com/helpcenter/" class="">Help Center</a></li>
+					<li><a href='cat=getting-started' class=''>Getting Started</a></li><li><a href='cat=creating-pages' class=''>Creating Pages</a></li><li><a href='cat=blogging' class=''>Blogging</a></li><li><a href='cat=popups' class=''>Popups</a></li><li><a href='cat=fonts-styles-colors' class=''>Fonts, Colors & Styles</a></li><li><a href='cat=integrating' class=''>Integrating with Other Platforms</a></li><li><a href='cat=domains' class=''>Domains</a></li><li><a href='cat=account' class=''>Account</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<h5 class="footer-title">Site Categories</h5>
+				<ul class="footer-links">
+					<li><a href="https://heroicnow.com/sites.php" class="">All Sites</a></li>
+					<li><a href="https://heroicnow.com/sites.php#coach" class="">Coaches</a></li>
+					<li><a href="https://heroicnow.com/sites.php#speaker" class="">Speaker Sites</a></li>
+					<li><a href="https://heroicnow.com/sites.php#author" class="">Author Sites</a></li>
+					<li><a href="https://heroicnow.com/sites.php#health" class="">Health & Fitness Sites</a></li>
+					<li><a href="https://heroicnow.com/sites.php#personalbrand" class="">Personal Brand Sites</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-6 col-md-4 col-lg-3">
+				<h5 class="footer-title">Company</h5>
+				<ul class="footer-links">
+					<li><a href="#" class="">About us</a></li>
+					<li><a href="#" class="">Contact</a></li>
+					<li><a href="/careers.php" class="">Careers</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
 @endif
 
 @include("partials.analytics")
